8de64d2c676fa3abfc1ffabccf08c0993d7113c2
Vinay <vinayvivek90@gmail.com>
2022-02-15 16:49:22 +0530

    single digit

--- sum.py
+++ sum.py
@@ -1,3 +1,5 @@
 def sum(str):
 	if str == "":
 		return 0
+	elif len(str) == 1 and str.isnumeric():
+		return int(str)
--- test_sum.py
+++ test_sum.py
@@ -3,3 +3,6 @@ from sum import sum
 
 def test_empty():
 	assert sum("") == 0
+
+def test_one():
+	assert sum('1') == 1
